09e0816eff8670b294673fef78243175873da317
shiyou63 <1747078780@example.com>
2025-03-30 21:40:08 +0800

    第一版，只有简单的浏览文章功能

--- bkxt-blog/src/main/java/com/ajuro/AjuroBlog.java
+++ bkxt-blog/src/main/java/com/ajuro/AjuroBlog.java
@@ -4,10 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author 35238
- * @date 2023/7/18 0018 19:13
- */
 
 @SpringBootApplication
 @MapperScan("com.ajuro.common.repository.mapper")
